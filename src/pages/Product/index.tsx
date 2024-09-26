07be621b3412a0d4016ca6f59e1f3e686502dce4
Joao Rampo <joao.rampoo@gmail.com>
2024-09-26 17:35:32 -0300

    new page

--- src/pages/Product/index.tsx
+++ src/pages/Product/index.tsx
@@ -1,3 +1,14 @@
-const Product = () => <div>produto</div>
+import { useParams } from 'react-router-dom'
+import Hero from '../../components/Hero'
+
+const Product = () => {
+  const { id } = useParams()
+
+  return (
+    <>
+      <Hero />
+    </>
+  )
+}
 
 export default Product
